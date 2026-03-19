19a4febe2830aaf6b8adf188e02daf824d73c657
Ana Paula Duarte <74932085+apduartte@users.noreply.github.com>
2026-03-19 20:48:06 -0300

    Update security groups for ALB and EC2 configurations

--- infra/terraform/envs/model1/security.tf
+++ infra/terraform/envs/model1/security.tf
@@ -1,25 +1,30 @@
-resource "aws_security_group" "poc_sg" {
-  name   = "poc-sg"
-  vpc_id = aws_vpc.poc_vpc.id
+# ALB aberto 443
+resource "aws_security_group" "alb_sg" {
+  vpc_id = aws_vpc.vpc.id
 
   ingress {
-    from_port   = 5678
-    to_port     = 5678
-    protocol    = "tcp"
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+# EC2 aceita apenas do ALB
+resource "aws_security_group" "ec2_sg" {
+  vpc_id = aws_vpc.vpc.id
 
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port       = 5678
+    to_port         = 5678
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
   }
 
   egress {
-    from_port   = 5678
-    to_port     = 5678
-    protocol    = "tcp"
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
