8e2a49cbd879ea5a8e6cf7b2914a09959aa89dc2
Ana Paula Duarte <74932085+apduartte@users.noreply.github.com>
2026-03-19 20:47:08 -0300

    Refactor VPC and subnet configuration in Terraform
    Updated VPC and subnet resources, removed unused resources.

--- infra/terraform/envs/model1/network.tf
+++ infra/terraform/envs/model1/network.tf
@@ -1,35 +1,18 @@
-resource "aws_vpc" "poc_vpc" {
+resource "aws_vpc" "vpc" {
   cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Name = "poc-vpc"
-  }
 }
 
-resource "aws_subnet" "poc_subnet" {
-  vpc_id                  = aws_vpc.poc_vpc.id
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.vpc.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
-
-  tags = {
-    Name = "poc-subnet"
-  }
 }
 
-resource "aws_internet_gateway" "poc_igw" {
-  vpc_id = aws_vpc.poc_vpc.id
+resource "aws_subnet" "private" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "10.0.2.0/24"
 }
 
-resource "aws_route_table" "poc_rt" {
-  vpc_id = aws_vpc.poc_vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.poc_igw.id
-  }
-}
-
-resource "aws_route_table_association" "poc_assoc" {
-  subnet_id      = aws_subnet.poc_subnet.id
-  route_table_id = aws_route_table.poc_rt.id
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
 }
