34dcd46ca2544df84155338cbfdb0e7df8eb80dd
apduartte <apduarttel@gmail.com>
2026-03-20 23:09:16 -0300

    refatorar(repo): reorganizar a estrutura do projeto para arquitetura DevOps e nuvem

--- terraform/backend/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "seu-bucket-terraform-state"
-    key            = "digiFusion/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-lock"
-    encrypt        = true
-  }
-}
